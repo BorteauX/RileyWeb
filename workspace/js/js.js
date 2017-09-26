e9f23d7986d51486409007ce918ddfff96b88050
Unknown <abcd50327@hotmail.com.tw>
2017-09-26 16:59:38 +0800

    [新增]info 文字動畫

--- workspace/js/js.js
+++ workspace/js/js.js
@@ -41,3 +41,14 @@ $(document).ready(function(){
 
     init();
 });
+
+$(document).ready(function(){
+    function info(){
+        var myResume = new SplitText(".info",{type:"words,chars"});
+        TweenMax.staggerFrom(myResume.chars, 0.5 , {opacity:0,rotation:-180,y:-100,ease: Back.easeOut} , 0.02)
+    }
+    function init(){
+        info();
+    }
+    init();
+});

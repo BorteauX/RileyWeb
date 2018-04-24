e61ca9b4e325d38decf0df1e5a464d4b2e4ef893
Unknown <abcd50327@hotmail.com.tw>
2018-04-24 09:22:18 +0800

    更新版面

--- js/js.js
+++ js/js.js
@@ -5,60 +5,75 @@
 })
 */
 
-$(document).ready(function(){
+$(document).ready(function()
+{
     // 底部動畫
-    function footer(){
-        var tl = new TimelineMax() ;
-        tl.from($('#js-copyright') , 2 , {
-            alpha:0 
+    function footer()
+    {
+        var tl = new TimelineMax();
+        tl.from($('#js-copyright'), 2,
+        {
+            alpha: 0
         });
     }
 
-    // 網頁載入一開始要做的動作
-    function init(){
-        footer();
-    }
-
-    init();
-});
-
-//大頭照動畫
-$(document).ready(function(){
-    function headshot(){
+    //大頭照動畫
+    function headshot()
+    {
         var tl2 = new TimelineMax();
-        tl2.from($('#js-logo') , 1 , {
-            scaleX:0.5,
-            scaleY:0.5,
+        tl2.from($('#js-logo'), 1,
+        {
+            scaleX: 0.5,
+            scaleY: 0.5,
             ease: Back.easeInOut
         });
 
-
-
     }
-    function init(){
+   
+   // 點LOGO 再跑一次動畫
+    var logo = document.getElementById('js-logo');
+    logo.onclick = function(){
         headshot();
     }
+    
 
-    init();
-});
 
-$(document).ready(function(){
-    function info(){
-        var myResume = new SplitText(".info",{type:"words,chars"});
-        TweenMax.staggerFrom(myResume.chars, 0.5 , {opacity:0, rotation:-180 ,ease: Back.easeInOut} , 0.02)
+
+
+    function info()
+    {
+        var myResume = new SplitText(".info",
+        {
+            type: "words,chars"
+        });
+        TweenMax.staggerFrom(myResume.chars, 0.5,
+        {
+            opacity: 0,
+            rotation: -180,
+            ease: Back.easeInOut
+        }, 0.02)
     }
-    function init(){
+
+
+
+
+    // var profile = $("#js-profile");
+    // $(document).scroll(function(e) {
+    //     if($(this).scrollTop() > $("#js-banner").height()) {
+    //         profile.css({"position" : "fixed", "top" : "0","right" : "0"});
+    //     } else {
+    //         profile.css("position", "relative");
+    //     }
+    // });
+
+
+    function init()
+    {
+        footer();
+        headshot();
         info();
     }
+
     init();
-});
 
-
-var profile = $("#js-profile");
-$(document).scroll(function(e) {
-    if($(this).scrollTop() > $("#js-banner").height()) {
-        profile.css({"position" : "fixed", "top" : "0","right" : "0"});
-    } else {
-        profile.css("position", "relative");
-    }
 });
